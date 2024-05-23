2677e33da53134b3ad08fb44e053ecef67fae162
Kíria Cássia de Oliveira <kiriacsoliveira@gmail.com>
2024-05-23 10:33:27 -0300

    feat: adjust baseUrl axios

--- src/app/components/Service/index.tsx
+++ src/app/components/Service/index.tsx
@@ -1,9 +1,16 @@
-import axios from "axios";
+import api from "./axios";
 
 export const GetProducts = async (page: number, rows: number, sortBy: string, orderBy: string) => {
   try {
-    const response = axios.get(`https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=${page}&rows=${rows}&sortBy=${sortBy}&orderBy=${orderBy}`);
-    return (await response).data;
+    const response = await api.get("/products", {
+      params: {
+        page,
+        rows,
+        sortBy,
+        orderBy,
+      },
+    });
+    return response.data;
   } catch (error) {
     throw new Error("Error fetching products");
   }
